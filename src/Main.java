139c684178e300981e96cad03d0fc548ab716175
Alexander Naumov <aleksnn00@gmail.com>
2021-03-13 18:12:14 +0500

    Finish cod

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 42414;
+        int part = 20;
+        int amount = 1;
+        int bonus = price / part * amount;
+        System.out.println(bonus);
+    }
+
+}
