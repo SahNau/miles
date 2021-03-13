f0c599ab67d73bce9ca1441cdc73a1d05dd4c32c
Alexander Naumov <77826434+SahNau@users.noreply.github.com>
2021-03-13 18:39:33 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int price = 42414;
         int part = 20;
-        int amount = 1;
         int bonus = price / part * amount;
         System.out.println(bonus);
     }
